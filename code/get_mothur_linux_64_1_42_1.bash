079c77acf8df675c90a72d7885663b1fcedcde16
Phil Richards <phil.richards@nottingham.ac.uk>
2019-05-20 14:33:03 +0100

    get contempory version of mothur

--- code/get_mothur_linux_64_1_42_1.bash
+++ code/get_mothur_linux_64_1_42_1.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget https://github.com/mothur/mothur/releases/download/v1.41.3/Mothur.linux_64.zip
+wget https://github.com/mothur/mothur/releases/download/v1.42.1/Mothur.linux_64.zip
 mv Mothur.linux_64.zip code/
 #Unzip into code/ directory as UoW may have bug in moving directories
 unzip code/Mothur.linux_64.zip -d code/
