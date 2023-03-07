5a67ba2ca81139496de8c7905af3e583c7da41fb
Phil Richards <Philip.Richards1@nottingham.ac.uk>
2023-03-07 13:44:36 +0000

    script has been superseded

--- code/get_mothur_linux_64_1_42_1.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://github.com/mothur/mothur/releases/download/v1.42.1/Mothur.linux_64.zip
-mv Mothur.linux_64.zip code/
-#Unzip into code/ directory as UoW may have bug in moving directories
-unzip code/Mothur.linux_64.zip -d code/
-#tidy up code/ folder
-rm -R code/__MACOSX/ code/Mothur.linux_64.zip
