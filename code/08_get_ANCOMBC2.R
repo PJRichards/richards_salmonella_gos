1dec40520a676226d0451cbb027e2ad1646eed5b
Phil Richards <Philip.Richards1@nottingham.ac.uk>
2023-05-18 12:41:14 +0100

    tidy up code and comments

--- code/08_get_ANCOMBC2.R
+++ code/08_get_ANCOMBC2.R
@@ -104,11 +104,9 @@ shared <- shared.raw %>%
                              pattern = "^[l]_[a-z]{3}_[a-z]{3}_\\d{2}_\\d")) %>% 
                       select(-c(label, numOtus)) 
 
-
 # drop columns with min reads cutoff
 abund.shared <- shared %>% 
                     select(where(~ any(.x >= min.depth)))
-
 
 # drop OTUs present in 10% communities 
 prev.shared <- abund.shared %>% 
@@ -142,14 +140,11 @@ design <- prev.shared %>%
                                challenge = as.factor(challenge)) %>% 
                         column_to_rownames(var = "Group") %>% 
                         select(c(diet, challenge, age))
-  
-
 
 # get matrix
 matrix <- prev.shared.long %>% 
                               column_to_rownames(var = "OTU") %>% 
                               as.matrix()
-
 
 # Continuous covariates “age”
 # Categorical covariates: “diet”, “challenge”
@@ -164,9 +159,7 @@ tse <- TreeSummarizedExperiment(assays=list(counts=matrix),
                                      colData=design
                                       )
 
-
-
-# Run ancombc2 function
+# Run ancombc2 function on 'late' trial
 output <- ancombc2(data = tse, 
                   assay_name = "counts", tax_level = tax_level,
                   fix_formula = "age + diet + challenge", 
@@ -194,29 +187,14 @@ output <- ancombc2(data = tse,
                   mdfdr_control = list(fwer_ctrl_method = "holm", B = 100)
                   )
 
-
 # Structural zeros
 tab_zero <- output$zero_ind
-
 
 # Sensitivity scores
 tab_sens <- output$pseudo_sens_tab
 
-
 # ANCOM-BC2 primary analysis
-# Result from the ANCOM-BC2 methodology to determine taxa that are differentially 
-# abundant according to the covariate of interest. 
-# Results contain: 
-#   1) log fold changes, 
-#   2) standard errors, 
-#   3) test statistics, 
-#   4) p-values, 
-#   5) adjusted p-values, 
-#   6) indicators of whether the taxon is differentially abundant (TRUE) 
-#      or not (FALSE).
 res_prim <- output$res
-
-
 
 ### Results for age ############################################################
 
@@ -225,29 +203,20 @@ df_age <- res_prim %>% select(taxon, ends_with("age"))
 
 df_fig_age <- fig_fix(df_age, diff_age, lfc_age)
 
-
 fig_age.p <- fig_plot(df_fig_age, lfc_age, se_age, 
                             "Log fold changes as one unit increase of age")
 
-
-
 # Pseudo-count sensitivity analysis for age
-# For the covariate of age, no outlying sensitivity scores are observed. 
-# All significant taxa have low sensitivity scores.
 sens_age <- sens_fix(tab_sens, df_age, diff_age, "sens_age", age) 
 
 fig_sens_age.p <- sens_plot(sens_age, sens_age, diff_age)
 
-
 # outputs
 write_tsv(x = sens_age, file = paste0(outpath,"age.tsv"))
-
 
 pdf(file = paste0(outpath,"age_plot.pdf"), paper = "a4", height = 9, width = 8)
 plot_grid(fig_age.p, fig_sens_age.p, ncol = 1, labels = c('A', 'B'))
 dev.off()
-
-
 
 ### Results for diet ######################################################
 
@@ -260,7 +229,7 @@ df_fig_diet <- fig_fix(df_diet, diff_dietgos, lfc_dietgos)
 fig_diet.p <- fig_plot(df_fig_diet, lfc_dietgos, se_dietgos, 
                            "Log fold changes associated with jGOS diet")
 
-# Pseudo-count sensitivity analysis for bmi
+# Pseudo-count sensitivity analysis for diet
 # All significant taxa have low sensitivity scores.??
 sens_diet <- sens_fix(tab_sens, df_diet, diff_dietgos, "sens_dietgos", dietgos) 
 
@@ -289,20 +258,15 @@ df_fig_challenge <- fig_fix(df_challenge, diff_challengeSE, lfc_challengeSE)
 fig_challenge.p <- fig_plot(df_fig_challenge, lfc_challengeSE, se_challengeSE, 
                                  "Log fold changes associated with SE challenge")
 
-
-
-# Pseudo-count sensitivity analysis for bmi
+# Pseudo-count sensitivity analysis for challenge
 # All significant taxa have low sensitivity scores.??
 sens_challenge <- sens_fix(tab_sens, df_challenge, diff_challengeSE, 
                              "sens_challengeSE", challengeSE) 
 
 fig_sens_challenge.p <- sens_plot(sens_challenge, sens_challengeSE, diff_challengeSE)
 
-
-
 # outputs
 write_tsv(x = df_challenge, file = paste0(outpath,"challenge.tsv"))
-
 
 pdf(file = paste0(outpath,"challenge_plot.pdf"), paper = "a4", height = 9, width = 8)
 plot_grid(fig_challenge.p, fig_sens_challenge.p, 
@@ -310,35 +274,19 @@ plot_grid(fig_challenge.p, fig_sens_challenge.p,
 dev.off()
 
 
-
-
 ### Bias-corrected abundances##################################################
-# Step 1: obtain estimated sample-specific sampling fractions (in log scale).
-#
-# Step 2: correct the log observed abundances by subtracting the estimated 
-#         sampling fraction from log observed abundances of each sample.
-#
-# Note that we are only able to estimate sampling fractions up to an additive 
-# constant. Thus, only the difference between bias-corrected abundances are 
-# meaningful.
 samp_frac <- output$samp_frac
-
 
 # Replace NA with 0
 samp_frac[is.na(samp_frac)] = 0 
 
-
 # Add pesudo-count (1) to avoid taking the log of 0
 log_obs_abn <- log(output$feature_table + 1)
-
 
 # Adjust the log observed abundances
 log_corr_abn <- t(t(log_obs_abn) - samp_frac)
 
-
 log_corr_abn_df <- log_corr.fix(log_corr_abn)
-
-
 
 # export
 write_tsv(x = log_corr_abn_df, file =  paste0(outpath, "bc_abundance.txt"))
